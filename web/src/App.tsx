b2d5e6cd1b1fdbb5d3f8105d61d13bd499281558
Danrley Lima <danrleylac@gmail.com>
2022-09-17 00:49:17 -0300

    componentinzando trechos

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,7 @@
 import { MagnifyingGlassPlus } from "phosphor-react"
 import logo from "./assets/logo.svg"
+import CreateAdBanner from "./components/CreateAdBanner"
+import { GameBanner } from "./components/GameBanner"
 import "./styles/main.css"
 
 function App() {
@@ -12,69 +14,16 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <a href="" className="relative rounded-lg overflow-hidden" >
-          <img src="./image 1.png" alt="" />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">League of Legends</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-
-        <a className="relative rounded-lg overflow-hidden" href="">
-          <img src="./image 2.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Dota 2</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-
-        <a className="relative rounded-lg overflow-hidden" href="">
-          <img src="./image 3.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Counter Strike</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-
-        <a className="relative rounded-lg overflow-hidden" href="">
-          <img src="./image 4.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">World of Warcraft</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-
-        <a className="relative rounded-lg overflow-hidden" href="">
-          <img src="./image 5.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Apex Legends</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-
-        <a className="relative rounded-lg overflow-hidden" href="">
-          <img src="./image 6.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Fortnite</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
+        <GameBanner bannerUrl="./image 1.png" title="League of Legends" adsCount={8} />
+        <GameBanner bannerUrl="./image 2.png" title="Dota 2" adsCount={4} />
+        <GameBanner bannerUrl="./image 3.png" title="CS:GO" adsCount={5} />
+        <GameBanner bannerUrl="./image 4.png" title="World of Warcraft" adsCount={3} />
+        <GameBanner bannerUrl="./image 5.png" title="Apex Legends" adsCount={2} />
+        <GameBanner bannerUrl="./image 6.png" title="Fortnite" adsCount={5} />
       </div>
 
-      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
-        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
-          <div>
-            <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
-            <span className="text-zinc-400 block">Publique um anúncio</span>
-          </div>
+      <CreateAdBanner />
 
-          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 transition-colors text-white rounded flex items-center gap-3">
-            <MagnifyingGlassPlus size={24}/>
-            Publicar anúncio
-          </button>
-        </div>
-      </div>
     </div>
   )
 }
