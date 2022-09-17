c851e636cd0230ad6b3c52f3167a7cf97a5ecdcf
Danrley Lima <danrleylac@gmail.com>
2022-09-17 12:52:56 -0300

    Fazendo query no backEnd para componentes do front

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,28 @@
-import { MagnifyingGlassPlus } from "phosphor-react"
+import { useState, useEffect } from 'react'
 import logo from "./assets/logo.svg"
 import CreateAdBanner from "./components/CreateAdBanner"
 import { GameBanner } from "./components/GameBanner"
 import "./styles/main.css"
 
+interface Game {
+  id: string,
+  title: string,
+  bannerUrl: string,
+  _count: {
+    ads: number,
+  }
+}
+
 function App() {
+  const [games, setGames] = useState<Game[]>([])
+
+  useEffect(() => {
+    fetch("http://localhost:3333/games")
+      .then(response => response.json())
+      .then(data => setGames(data))
+  }, [])
+
+
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logo} alt="logo" />
@@ -13,13 +31,17 @@ function App() {
         Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui
       </h1>
 
+
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <GameBanner bannerUrl="./image 1.png" title="League of Legends" adsCount={8} />
-        <GameBanner bannerUrl="./image 2.png" title="Dota 2" adsCount={4} />
-        <GameBanner bannerUrl="./image 3.png" title="CS:GO" adsCount={5} />
-        <GameBanner bannerUrl="./image 4.png" title="World of Warcraft" adsCount={3} />
-        <GameBanner bannerUrl="./image 5.png" title="Apex Legends" adsCount={2} />
-        <GameBanner bannerUrl="./image 6.png" title="Fortnite" adsCount={5} />
+        {games.map(game => {
+          return (
+            <GameBanner
+              key={game.id}
+              bannerUrl={game.bannerUrl}
+              title={game.title}
+              adsCount={game._count.ads} />
+          )
+        })}
       </div>
 
       <CreateAdBanner />
